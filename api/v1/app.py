2a15e3238e90b1e9fefbc865f9cd573573ec52a2
el batouri rabi <elbatouri@gmail.com>
2024-01-08 13:38:14 -0800

    fix app.py by removing not used models for now

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,13 +4,10 @@ from models import storage
 from api.v1.views import app_views
 from os import environ
 from flask import Flask, make_response, jsonify
-from flask_cors import CORS
-from flasgger.utils import swag_from
 
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
